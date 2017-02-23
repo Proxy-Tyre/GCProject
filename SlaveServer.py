d4a309d3aad133629668da2aab2c01817399984a
zyssky <2462084760@qq.com>
2017-02-23 12:04:53 +0800

    添加设置host地址，端口和接受统计信息地址，端口

--- SlaveServer.py
+++ SlaveServer.py
@@ -5,6 +5,7 @@ import subprocess
 import multiprocessing
 import json
 import time
+import argparse
 from urllib import request
 from IOfunc import *
 
@@ -34,22 +35,32 @@ class Server(object):
 		self.domain_status={}             # {domain:[status,access_count,stick_time,start_time]}
 		self.domain_status_judge_table={}       # {domain:[(success_url,200,length),(second_last_url,httpcode,length),(last_url,httpcode,length)]}
 		
-		self.HOST = '127.0.0.1'
-		self.PORT = 2888
+		self.set_addr_port()
+
 
 		threading.Thread(target=self.debug).start()
 
 		threading.Thread(target=self.report,args=(interval_time,)).start()
 
-	def set_host_port(self,host,port):
-		self.HOST = host
-		self.PORT = port
+	def set_addr_port(self):
+		parser = argparse.ArgumentParser()
+		parser.add_argument("--host_addr", default="127.0.0.1", help="Default: 127.0.0.1")
+		parser.add_argument("--host_port", default="2888", help="Default: 2888")
+		parser.add_argument("--mesg_addr", default="127.0.0.1", help="Default: 127.0.0.1")
+		parser.add_argument("--mesg_port", default="10044", help="Default: 10044")
+		args = parser.parse_args()
+		self.host_addr = args.host_addr
+		self.host_port = args.host_port
+		self.mesg_port = args.mesg_port
+		self.mesg_addr = args.mesg_addr
+		print('log: host addr ',self.host_addr)
+		print('log: host port ',self.host_port)
 
 	def connect(self):
 		while True:
 			try:
 				sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-				sock.connect((self.HOST,self.PORT))
+				sock.connect((self.host_addr,self.host_port))
 				return sock
 			except:
 				continue
@@ -99,7 +110,7 @@ class Server(object):
 			
 			time.sleep(interval_time)
 
-	def ping_for_windows(self,host): 
+	def ping(self,host): 
 		cmd_status,cmd_result = subprocess.getstatusoutput('ping '+host)
 		if not cmd_status:
 			p = re.compile('时间.(\d+?)ms')
@@ -112,12 +123,9 @@ class Server(object):
 			
 	# below are used to update the doamin_dalay
 	def evaluate(self,host):
-		t = ping(host)
+		t = self.ping(host)
 		if t:
-			r = 0
-			for d in t:
-				r+=d
-			self.domain_delay[host] = int(r/len(t))
+			self.domain_delay[host] = int(t)
 		else:
 			self.domain_delay[host] = TIMEOUT
 
@@ -132,70 +140,68 @@ class Server(object):
 			thread.join()
 
 
-	# below are used to receive the 
+	# below are used to receive the message
 	def recv_access_message(self):
-		ip = "127.0.0.1"
-		port = 10044
 		listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		listener.bind((ip, port))
+		listener.bind((self.mesg_addr, int(self.mesg_port)))
 		listener.listen(100)
-		print('ready to receive message')
+		print('log: message addr ',self.mesg_addr)
+		print('log: message port ',self.mesg_port)
 
 		while True:
 			try:
 				conn, addr = listener.accept()
 				print("connect from %s:%d" % addr)
-				data = recv_data(conn,BUFSIZE)
-				for datatype in data:
-					if datatype == DataType.CONNECTION:
-						threading.Thread(target=self.handle_access_message,args=(data[datatype],)).start()
+				
+				while True:
+					data = conn.recv(8192)
+					if not data:
+						break
+					conn.send(b"OK")
+					try:
+						data = data.decode()
+						url, code, contentLen = data.split("\r\n")
+						if code:
+							code = int(code)
+						else:
+							code = 0
+						if contentLen:
+							contentLen = int(contentLen)
+						else:
+							contentLen = 0
+						threading.Thread(target=self.handle_access_message,args=(url,code,contentLen)).start()
+					except Exception as e:
+						pass
 			except KeyboardInterrupt:
 				break
 			except Exception as e:
 				print(e)
 
 
-	def handle_access_message(self,data):
-		for connectiontype in data:
-			if connectiontype == ConnectionType.SUCCESS:
-				self._handle_success_connection_message(data[connectiontype])
-			if connectiontype == ConnectionType.CONNECTED:
-				self._handle_connected_connection_message(data[connectiontype])
-			
-			
-	def _add_count_to_record(self,domain):
-		status_list = self.domain_status[domain]
-		status_list[1]+=1
-		status_list[2] = round(time.time())-status_list[3]
-
-
-	def _refresh_domain_status(self,domain):
-		self.domain_status[domain]=[]
-		self.domain_status[domain].append(STATUS_OK)
-		self.domain_status[domain].append(1)
-		self.domain_status[domain].append(0)
-		self.domain_status[domain].append(round(time.time()))
-
-
-	def _handle_success_connection_message(self,datalist):
-		url = datalist[0]
-		code = datalist[1]
-		contentLen = datalist[2]
+	def handle_access_message(self,url,code,contentLen):
+		if contentLen == None:
+			contentLen = 0
 		domain = get_domain(url)
 		if domain:
-			if domain in self.domain_status and not self.recode_is_timeout(self.domain_status[domain]):
-				self._add_count_to_record(domain)
+			if domain in self.domain_status:
+				if not self.recode_is_timeout(self.domain_status[domain]):
+					status_list = self.domain_status[domain]
+					# update_domain_status_judge_table(data_list[0],data_list[1],data_list[2])
+					status_list[1]+=1
+					status_list[2] = round(time.time())-status_list[3]
+				else:
+					self.domain_status[domain]=[]
+					self.domain_status[domain].append(STATUS_OK)
+					self.domain_status[domain].append(1)
+					self.domain_status[domain].append(0)
+					self.domain_status[domain].append(round(time.time()))					
 			else:
-				self._refresh_domain_status(domain)
-			self.update_domain_status_judge_table(url,code,contentLen)
-
-
-
-	def _handle_connected_connection_message(self,url):
-		domain = get_domain(url)
-		if domain:
-			self._add_count_to_record(domain)
-
+				self.domain_status[domain]=[]
+				self.domain_status[domain].append(STATUS_OK)
+				self.domain_status[domain].append(1)
+				self.domain_status[domain].append(0)
+				self.domain_status[domain].append(round(time.time()))
+			self.update_domain_status_judge_table(url,code,contentLen)		
 
 
 	def recode_is_timeout(self,status_list):
@@ -243,9 +249,6 @@ class Server(object):
 		if url != self.domain_status_judge_table[domain][2][0] and code == self.domain_status_judge_table[domain][2][1] and content_length < self.domain_status_judge_table[domain][2][2]+PRECISION and content_length > self.domain_status_judge_table[domain][2][2]-PRECISION:
 			if url != self.domain_status_judge_table[domain][1][0] and code == self.domain_status_judge_table[domain][1][1] and content_length < self.domain_status_judge_table[domain][1][2]+PRECISION and content_length > self.domain_status_judge_table[domain][1][2]-PRECISION:
 				if url != self.domain_status_judge_table[domain][0][0] and code == self.domain_status_judge_table[domain][0][1] and content_length < self.domain_status_judge_table[domain][0][2]+PRECISION and content_length > self.domain_status_judge_table[domain][0][2]-PRECISION:
-					status = self.domain_status[domain]
-					status[0] = STATUS_NO
-					status[2] = time.time()-status[3]					
 					return False
 				elif not self.check_the_same_with_success_url(domain,url,code,content_length):
 					status = self.domain_status[domain]
